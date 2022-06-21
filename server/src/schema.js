cafe856e03e6b2bb6bc32e562fa45c25718d0f0b
RafaPalau <rafaeldonizetip@gmai.com>
2022-06-21 00:52:33 -0300

    add live data, resolvers and data sources

--- server/src/schema.js
+++ server/src/schema.js
@@ -5,28 +5,26 @@ const typeDefs = gql`
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
   }
-
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
-    "the track's title"
+    "The track's title"
     title: String!
-    "the track's main author"
+    "The track's main Author"
     author: Author!
-    "the track's main illustration to display in track card or track page detail"
+    "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "the track's approximate lengthto complete in minutes"
+    "The track's approximate length to complete, in minutes"
     length: Int
-    "the number of modules this track contains"
+    "The number of modules this track contains"
     modulesCount: Int
   }
-
-  "Author of a complete Track"
+  "Author of a complete Track or a Module"
   type Author {
     id: ID!
-    "the author's name"
+    "Author's first and last name"
     name: String!
-    "Author's profile picture url"
+    "Author's profile picture"
     photo: String
   }
 `;
