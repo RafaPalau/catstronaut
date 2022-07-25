f48beed6dc402681bf834d9cdb8b7f3a88b38207
RafaPalau <rafaeldonizetip@gmai.com>
2022-07-25 09:12:44 -0300

    Adjustment footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -8,7 +8,7 @@ import { colors, ApolloIcon } from '../styles';
 const Footer = ({ children }) => {
   return (
     <FooterContainer>
-      2022 ©{' '}
+      2022 Copyright ©{' '}
       <LogoContainer>
         <ApolloIcon width="100px" height="40px" />
       </LogoContainer>
